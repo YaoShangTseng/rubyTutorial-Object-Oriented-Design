a7eb95de5f93b3e89013e2a9df11a95273369b6b
YaoShangTseng <yakushou730@gmail.com>
2016-12-26 17:05:19 +0800

    Chapter2 - 2nd example

--- test.rb
+++ test.rb
@@ -1,4 +1,7 @@
 require_relative "gear"
 
-puts Gear.new(52, 11).ratio
-puts Gear.new(30, 27).ratio
+#puts Gear.new(52, 11).ratio
+#puts Gear.new(30, 27).ratio
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(52, 11, 24, 1.25).gear_inches
