087c996841c7eb48e8385b3c59ea8e6404e0c7c4
YaoShangTseng <yakushou730@gmail.com>
2016-12-26 18:25:19 +0800

    Chapter2 - 3rd example

--- gear.rb
+++ gear.rb
@@ -1,11 +1,10 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
   end
 
   def ratio
@@ -13,7 +12,13 @@ class Gear
   end
 
   def gear_inches
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
+  end
+
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
   end
 
 end
--- test.rb
+++ test.rb
@@ -1,7 +1,16 @@
 require_relative "gear"
+require_relative "wheel"
 
 #puts Gear.new(52, 11).ratio
 #puts Gear.new(30, 27).ratio
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
+#puts Gear.new(52, 11, 26, 1.5).gear_inches
+#puts Gear.new(52, 11, 24, 1.25).gear_inches
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inches
+
+puts Gear.new(52, 11).ratio
+
